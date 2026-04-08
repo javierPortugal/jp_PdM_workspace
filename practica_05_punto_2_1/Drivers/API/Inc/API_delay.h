5d2f636729c440d83e257a1bc6da4bb75c9c3e6e
Javier Portuga <javier.s.portugal@gmail.com>
2026-04-08 15:28:21 -0600

    update to practica 5 point 2
    update on project for point 2

---
+++ practica_05_punto_2_1/Drivers/API/Inc/API_delay.h
@@ -0,0 +1,39 @@
+/*
+ * API_delay.h
+ *
+ *  Created on: 19 mar 2026
+ *      Author: Javier Portugal
+ */
+
+#ifndef API_INC_API_DELAY_H_
+#define API_INC_API_DELAY_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+typedef uint32_t tick_t; // Qué biblioteca se debe incluir para que esto compile? stdint.h
+typedef bool bool_t;  // Qué biblioteca se debe incluir para que esto compile? stdbool.h
+typedef uint32_t entero32;
+
+typedef enum
+{estado_A, estado_B, estado_C, estado_D
+}estado_t;
+
+typedef struct{
+   tick_t startTime;
+   tick_t duration;
+   bool_t running;
+} delay_t;
+
+void delayInit( delay_t * delay, tick_t duration );
+bool_t delayRead( delay_t * delay );
+void delayWrite( delay_t * delay, tick_t duration );
+
+bool_t delayIsRunning( delay_t* delay );
+
+
+
+
+
+#endif /* API_INC_API_DELAY_H_ */
